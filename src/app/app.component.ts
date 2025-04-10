a8671199e0cff1606de8ecfe0c4410292a0a3d7e
kannanATexcelforte <kannan@excelforte.com>
2025-04-10 07:40:25 +0530

    10.04.2025
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  imports: [CommonModule, FormsModule],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'book-app1';
+  projectTitle = 'Book Application!!';
+  myColor = 'yellow';
+  status = true;
+  className = '';
+  bookName = 'Angular';
+  message = 'great';
+
+  showData($event: any){
+    this.className = 'rainbow-btn';
+  }
 }
