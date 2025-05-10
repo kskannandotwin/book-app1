8993ca46f83e172621054bc205dc13284fe40d39
kannanATexcelforte <kannan@excelforte.com>
2025-05-10 14:22:58 +0530

    10.05.2025
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  title = 'book-app1';
+  bookName = 'Angular programming';
 }
