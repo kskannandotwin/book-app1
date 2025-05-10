6c1188349d484e9b72cc8286e0f958abebf6f0e7
kannanATexcelforte <kannan@excelforte.com>
2025-05-10 15:44:18 +0530

    10.05.2025
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { FormControl, FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, ReactiveFormsModule],
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'book-app1';
-  bookName = 'Angular programming';
+  bookForm = new FormGroup({
+    bookName: new FormControl(''),
+    bookAuthor: new FormControl(''),
+    bookPrice: new FormControl(''),
+    bookDetails: new FormGroup({
+      chapters: new FormControl(''),
+      pages: new FormControl('')
+    })
+  });
+
+  public onSubmit() {
+    this.bookForm.patchValue({
+      bookName: 'Angular',
+      bookDetails: {
+        chapters: '8',
+        pages: '500'
+      }
+    })
+  }
 }
