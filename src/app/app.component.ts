8a3bca19562d191f560c10b16496f24de6e794c4
kannanATexcelforte <kannan@excelforte.com>
2025-05-12 04:38:22 +0530

    12.05.2025
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { BookDetailComponent } from './book-detail/book-detail.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [CommonModule, FormsModule, BookDetailComponent],
+  imports: [ReactiveFormsModule],
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  projectTitle = 'Book Application!!';
-  myColor = 'yellow';
-  public bookName = 'Learn Angular';
+  title = 'book-app1';
+
+  private fb = inject(FormBuilder);
+
+  bookForm = this.fb.group({
+    bookName: [''],
+    bookAuthor: ['', Validators.required],
+    bookPrice: [''],
+    bookDetails: this.fb.group({
+      chapters: [''],
+      pages: [''],
+    }),
+  });
+
+  public onSubmit() {
+    this.bookForm.patchValue({
+      bookName: 'Angular',
+      bookDetails: {
+        chapters: '8',
+        pages: '500',
+      },
+    });
+    console.log('submit called');
+    console.log(this.bookForm.value);
+  }
 }
