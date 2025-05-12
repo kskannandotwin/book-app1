a6d74682a2fb76c6a09ea9405a8f231ffafe1af9
kannanATexcelforte <kannan@excelforte.com>
2025-05-12 12:03:08 +0530

    12.05.2025
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
@@ -12,7 +12,10 @@ export class AppComponent {
   title = 'book-app1';
   private fb = inject(FormBuilder);
   bookForm = this.fb.group({
-    bookName: [''],
+    bookName: ['', [Validators.required, Validators.minLength(10), myValidation]],
+    authorGroup: this.fb.group({
+      bookAuthor: [''],
+      confirmBookAuthor: ['']}, { validators: confirmAuthor }),
     bookAuthor: ['', Validators.required],
     bookPrice: [''],
     bookDetails: new FormArray([
@@ -38,5 +41,24 @@ export class AppComponent {
     })
     console.log('submit called');
     console.log(this.bookForm.value);
+  }
+}
+
+function myValidation(control: AbstractControl): { [key: string]: any } | null {
+  const data: string = control.value;
+  if (data === '' || data.startsWith('Ang')) {
+    return null;
+  }
+  return { 'nameerror': true };
+}
+
+function confirmAuthor(group: AbstractControl): { [key: string]: any } | null {
+  console.log('In confirm author');
+  const author1 = group.get('bookAuthor');
+  const author2 = group.get('confirmBookAuthor');
+  if (author1?.value === author2?.value) {
+    return null;
+  } else {
+    return { 'authormismatch': true };
   }
 }
