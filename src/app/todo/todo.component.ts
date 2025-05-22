f67d498cf3bcdb54d1b3aa8182ee8fb8d819e264
kannanATexcelforte <kannan@excelforte.com>
2025-05-22 07:08:34 +0530

    22.05.2025
    completed

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,23 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { TodoService } from '../todo.service';
-import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate } from '@angular/animations'; // <-- Add this
 @Component({
   selector: 'app-todo',
   imports: [FormsModule, CommonModule],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.scss'
+  styleUrl: './todo.component.scss',
+    animations: [
+    trigger('taskAnim', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate(
+          '300ms ease-out',
+          style({ transform: 'translateX(0)', opacity: 1 })
+        ),
+      ]),
+      transition(':leave', [
+        animate(
+          '300ms ease-in',
+          style({ transform: 'translateX(-100%)', opacity: 0 })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class TodoComponent {
-  newTodo!: string;
-  constructor(public todoService: TodoService) { }
+  task: string = '';
+  tasks: string[] = [];
 
-  addTodo(title: string) {
-    this.todoService.addTodo(title);
+  addTask() {
+    if (this.task && this.task.trim() !== '') {
+      this.tasks.push(this.task.trim());
+      this.task = '';
+    }
   }
 
-  removeTodo(index: number) {
-    this.todoService.removeTodo(index);
+  removeTask(index: number) {
+    if (index > -1 && index < this.tasks.length) {
+      this.tasks.splice(index, 1);
+    }
   }
-
 }
